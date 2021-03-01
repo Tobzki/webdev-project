2c45bd45f1ad2867996fa567410a8ccf1ba93c7c
Tobias Andersson <tobiasgustaverik@icloud.com>
2021-03-01 15:38:13 +0100

    Refactoring + backend

--- graphql/schema/index.js
+++ graphql/schema/index.js
@@ -26,6 +26,12 @@ type User {
   createdEvents: [Event!]
 }
 
+type AuthData {
+    userId: ID!
+    token: String!
+    tokenExpiration: Int!
+}
+
 input UserInput {
 
   email: String!
@@ -43,6 +49,7 @@ input EventInput {
 type RootQuery { 
     events: [Event!]!
     bookings: [Booking!]!
+    login(email: String!, password: String!): AuthData!
 }
 
 type RootMutation {
---
+++ middleware/is-auth.js
@@ -0,0 +1,27 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = (req, res, next) => {
+  const authHeader = req.get("Authorization");
+  if (!authHeader) {
+    req.isAuth = false;
+    return next();
+  }
+  const token = authHeader.split(" ")[1];
+  if (!token || token === " ") {
+    req.isAuth = false;
+    return next();
+  }
+
+  try {
+    decodedToken = jwt.verify(token, "somesupersecretkey");
+  } catch (err) {
+    req.isAuth = false;
+    throw next();
+  }
+
+  if (!decodedToken) {
+    req.isAuth = true;
+    req.userId = decodedToken.userId;
+    next();
+  }
+};
