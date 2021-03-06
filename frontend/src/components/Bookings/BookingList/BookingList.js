9686b4a392b2d4f4ba35faee85e1daeabf8ae1cc
Håkan Reinholdson <hreinholdson@hotmail.com>
2021-03-06 17:40:56 +0100

    Added cancel bookings

--- frontend/src/components/Bookings/BookingList/BookingList.js
+++ frontend/src/components/Bookings/BookingList/BookingList.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import './BookingList.css';
 
 const bookingList = props => (
 
-    <ul>
+    <ul className="bookings_list">
         {props.bookings.map(booking => {
             return (
-            <li>
+            <li key={booking._id} className="bookings_item">
+                <div className="bookings_item-data">
                  {booking.event.title} - {' '} 
             {new Date(booking.createdAt).toLocaleDateString()}
+            </div>
+            <div className="bookings_item-actions">
+                <button className="btn" onClick={props.onDelete.bind(this, booking._id)}>Cancel</button>
+            </div>
             </li>
             );
         })}
