7f25107a81f4e6ccd7600c25ef104a17c84bbde7
Tobias Andersson <tobiasgustaverik@icloud.com>
2021-03-08 14:53:25 +0100

    Barcharts and list & chart buttons

--- graphql/resolvers/merge.js
+++ graphql/resolvers/merge.js
@@ -14,6 +14,10 @@ const userLoader = new Dataloader(userIds => {
 const events = async eventIds => {
   try {
     const events = await Event.find({ _id: { $in: eventIds } });
+    events.sort((a, b) => {
+      return eventIds.indexOf(a._id.toString()) - eventIds.indexOf(b._id.toString())
+    });
+    console.log(events, eventIds)
     return events.map(event => {
       return transformEvent(event);
     });
