1ceb30c29bd5294f4cf57c8ee2729fa3b52083d6
Håkan Reinholdson <hreinholdson@hotmail.com>
2021-03-12 09:59:17 +0100

    Added MyPage start

---
+++ frontend/src/pages/MyPage.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import "./MyPage.css";
+
+
+const method = () => {
+  return (
+    <React.Fragment>
+        <form>
+        <div className="myPage">
+        <h1>Medlemssidan här, testing Git</h1>
+        </div>
+        </form>
+    </React.Fragment>
+  );
+};
+
+export default method;
