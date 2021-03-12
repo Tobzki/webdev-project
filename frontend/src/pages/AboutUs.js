b6fd358e34b3e06bb3495e498c98c58c7549cbfa
Tobias Andersson <tobiasgustaverik@icloud.com>
2021-03-12 12:21:10 +0100

    aboutus page

--- frontend/src/pages/AboutUs.js
+++ frontend/src/pages/AboutUs.js
@@ -3,13 +3,19 @@ import "./AboutUs.css";
 
 const method = () => {
   return (
-    <React.Fragment>
-        <form>
-        <div className="aboutus">
-        <h1>HEJSAN ALLESAMMANS</h1>
-        </div>
-        </form>
-    </React.Fragment>
+    <div className="body">
+      <div className="card">
+        <h1>Tobias Andersson</h1>
+        <p>Back End Developer</p>
+        <img src="images/tobzki.jpg" alt="me"></img>
+      </div>
+
+      <div className="cardtwo">
+        <h1>Håkan Reinholdsson</h1>
+        <p>Front End Developer</p>
+        <img src="images/håkzki.jpg" alt="me"></img>
+      </div>
+    </div>
   );
 };
 
